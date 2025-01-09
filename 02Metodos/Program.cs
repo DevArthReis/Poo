a22e746d162ef780f814d41f0920b44ed9f07cb1
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2025-01-09 17:04:53 -0300

    Att

--- 02Metodos/Program.cs
+++ 02Metodos/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace _02Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa obj = new Pessoa();
+            
+            obj.apresentacao("Arthur", 20);
+        }
+    }
+}
