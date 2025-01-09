88c7d829cfd35c261a688536079e3b7f16bbabff
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2025-01-09 16:50:43 -0300

    Att

---
+++ 01Conceitos/Usuario.cs
@@ -0,0 +1,13 @@
+class Usuario
+{
+    public int id;
+    public string name;
+    public string email;
+    public string password;
+    public void resumo(){
+        Console.WriteLine("Id:"+id+"Nome: "+name+" Email: "+email+" Senha:"+password);
+    }
+
+    
+     
+}
