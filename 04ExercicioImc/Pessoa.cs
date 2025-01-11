3b40db2dcfd53652513732bb6bdbda23b4137452
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2025-01-11 17:55:51 -0300

    Att

---
+++ 04ExercicioImc/Pessoa.cs
@@ -0,0 +1,11 @@
+class Pessoa
+{
+    public string nome;
+    public float peso;
+    public float altura;
+    public double CalcularIMC()
+    {
+        return peso / (altura * altura);
+    }
+
+}
---
+++ 04ExercicioImc/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _04ExercicioImc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa obj = new Pessoa();
+            obj.nome = "Arthur";
+            obj.peso = 89.5f;
+            obj.altura = 1.87f;
+            Console.WriteLine($"Nome: {obj.nome}");
+            Console.WriteLine($"Peso: {obj.peso} kg");
+            Console.WriteLine($"Altura: {obj.altura} m");
+            double imc = obj.CalcularIMC();
+            Console.WriteLine($"IMC: {imc:F2}");
+            if (imc < 18.5f)
+            {
+                Console.WriteLine("Abaixo do peso");
+            }
+            else if (imc < 25)
+            {
+                Console.WriteLine("Peso normal");
+            }
+            else if (imc < 30)
+            {
+                Console.WriteLine("Acima do peso");
+            }
+            else if (imc < 35)
+            {
+                Console.WriteLine("Obesidade 1");
+            }
+            else if (imc < 40)
+            {
+                Console.WriteLine("Obesidade 2");
+            }
+            else
+            {
+                Console.WriteLine("Obesidade 3");
+            }
+
+
+
+        }
+    }
+}
