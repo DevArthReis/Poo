532e69280ba35ed750256982625009e9cc80f557
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2025-01-14 18:45:56 -0300

    Att

---
+++ 09GetSet/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _09GetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa obj = new Pessoa();
+            obj.Nome=Console.ReadLine();//Set recebe o valor e armazena
+            Console.WriteLine(obj.Nome);//Get obtém o valor armazenado e envia o valor para a tela do console.
+
+        }
+    }
+}
