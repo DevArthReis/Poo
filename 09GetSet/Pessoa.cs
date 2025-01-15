eda42dfe7b4a538475dad3c895eb3874d329713b
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2025-01-15 17:53:50 -0300

    Att

--- 09GetSet/Pessoa.cs
+++ 09GetSet/Pessoa.cs
@@ -2,11 +2,18 @@ class Pessoa
 {
     //Atributo nome
     private string nome;
+    //Atributo senha 
+    private double senha;
     //Get Set todo metodo Get e Set tem que ser publicos
     public string Nome 
     { 
         get{return nome;}
         set{nome = value;} 
+    }
+    public double Senha
+    {
+        get{ return senha;}
+        set{ senha = value;}
     }
 
 }
